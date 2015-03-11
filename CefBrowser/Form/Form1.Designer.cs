cbba1c98c80f388b1dd572cd430531aa944c3a6d
win <wintercoredev@gmail.com>
2015-03-11 17:29:04 +0700

    03: CefBrowser

---
+++ CefBrowser/Form/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace CefBridgeTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.cefWebBrowser1 = new LayoutFarm.CefBridge.CefWebBrowser();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(132, 37);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Navigate";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // cefWebBrowser1
+            // 
+            this.cefWebBrowser1.Location = new System.Drawing.Point(162, 12);
+            this.cefWebBrowser1.Name = "cefWebBrowser1";
+            this.cefWebBrowser1.Size = new System.Drawing.Size(802, 531);
+            this.cefWebBrowser1.TabIndex = 6;
+            this.cefWebBrowser1.Text = "cefWebBrowser1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 555);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.cefWebBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LayoutFarm.CefBridge.CefWebBrowser cefWebBrowser1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ CefBrowser/Form/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+
+using System.Text;
+using System.Windows.Forms;
+
+namespace CefBridgeTest
+{
+    public partial class Form1 : Form
+    {
+
+        Timer tt = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            tt.Tick += new EventHandler(tt_Tick);
+
+            //=======================
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            tt.Enabled = false;
+
+
+            this.cefWebBrowser1.PrepareNativeClose();
+            base.OnClosing(e);
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            //tt.Enabled = true;
+            base.OnLoad(e);
+        }
+        public class MyConsole
+        {
+            string myname;
+            public MyConsole(string myname)
+            {
+                this.myname = myname;
+            }
+            public string MyName
+            {
+                get
+                {
+                    return this.myname;
+                }
+                set
+                {
+                    this.myname = value;
+                }
+            }
+            /// <summary>
+            /// expose to javascript
+            /// </summary>
+            /// <param name="str"></param>
+            public void Log(string str)
+            {
+                Console.WriteLine(str);
+            }
+        }
+        void TestMe001()
+        {
+
+        }
+
+
+        void tt_Tick(object sender, EventArgs e)
+        {
+            LayoutFarm.CefBridge.Cef3Binder.MyCefDoMessageLoopWork();
+            LayoutFarm.CefBridge.Cef3Binder.MyCefDoMessageLoopWork();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.cefWebBrowser1.NavigateTo("http://localhost/html5/mycanvas.html");
+
+        }
+
+    }
+
+}
