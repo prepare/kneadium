b6997ab1a71f7c98b4b1d0bd75f7b191bf54c807
win <wintercoredev@gmail.com>
2015-05-12 18:43:07 +0700

    25: MyCefCreateAppClient()

--- cef3/cefclient/ExportFuncs.cpp
+++ cef3/cefclient/ExportFuncs.cpp
@@ -2,6 +2,18 @@
 #include "mycef.h"
 //static 
 
+#include "include/base/cef_scoped_ptr.h"
+#include "include/cef_command_line.h"
+#include "include/cef_sandbox_win.h"
+#include "cefclient/browser/client_app_browser.h"
+#include "cefclient/browser/main_context_impl.h"
+#include "cefclient/browser/main_message_loop_multithreaded_win.h"
+#include "cefclient/browser/main_message_loop_std.h"
+#include "cefclient/browser/root_window_manager.h"
+#include "cefclient/browser/test_runner.h"
+#include "cefclient/common/client_app_other.h"
+#include "cefclient/renderer/client_app_renderer.h"
+
 delTraceBack notifyListener= NULL;
 
 //1.
@@ -30,3 +42,26 @@ int RegisterManagedCallBack(void* funcPtr,int callbackKind)
 	} 
 	return 1;
 }
+//3.
+client::ClientApp* MyCefCreateClientApp()
+{	
+	// Parse command-line arguments.
+   CefRefPtr<CefCommandLine> command_line = CefCommandLine::CreateCommandLine();
+   command_line->InitFromString(::GetCommandLineW());
+	//create browser process first?
+   client::ClientApp* app= NULL;
+   client::ClientApp::ProcessType process_type = client::ClientApp::GetProcessType(command_line);
+   if (process_type == client::ClientApp::BrowserProcess)
+     app = new client::ClientAppBrowser();
+   else if (process_type == client::ClientApp::RendererProcess)
+     app = new client::ClientAppRenderer();
+   else if (process_type == client::ClientApp::OtherProcess)
+     app = new client::ClientAppOther();
+   return app;
+}
+
+int MyCefInit(HINSTANCE hInstance,client::ClientApp* app)
+{
+	CefRefPtr<client::ClientApp> myApp = app;   
+	return DllInitMain(hInstance,myApp);
+} 
--- cef3/cefclient/ExportFuncs.h
+++ cef3/cefclient/ExportFuncs.h
@@ -1,8 +1,13 @@
+#include "dll_init.h"
+#include "common/client_app.h"
 #define MY_DLL_EXPORT __declspec(dllexport)  
+ 
+
 extern "C"{  	 
 
 	MY_DLL_EXPORT int MyCefGetVersion(); 
 	MY_DLL_EXPORT int RegisterManagedCallBack(void* callback,int callBackKind); 
-
+	MY_DLL_EXPORT client::ClientApp* MyCefCreateClientApp();
+	MY_DLL_EXPORT int MyCefInit(HINSTANCE hInstance,client::ClientApp* app);
 
 }
