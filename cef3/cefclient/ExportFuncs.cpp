0fab7bce12de66bdb8d76f502f63ac93d57a00e1
win <wintercoredev@gmail.com>
2015-05-20 15:40:26 +0700

    55: merge

--- cef3/cefclient/ExportFuncs.cpp
+++ cef3/cefclient/ExportFuncs.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "ExportFuncs.h"   
 #include "mycef.h"
 //static 
@@ -69,85 +68,12 @@ client::ClientApp* MyCefCreateClientApp()
    else if (process_type == client::ClientApp::OtherProcess)
      app = new client::ClientAppOther();
 
-=======
-#include "ExportFuncs.h"   
-#include "mycef.h"
-//static 
-
-#include "include/base/cef_scoped_ptr.h"
-#include "include/cef_command_line.h"
-#include "include/cef_sandbox_win.h"
-#include "cefclient/browser/client_app_browser.h"
-#include "cefclient/browser/main_context_impl.h"
-#include "cefclient/browser/main_message_loop_multithreaded_win.h"
-#include "cefclient/browser/main_message_loop_std.h"
-#include "cefclient/browser/root_window_win.h"
-#include "cefclient/browser/root_window_manager.h"
-#include "cefclient/browser/test_runner.h"
-#include "cefclient/common/client_app_other.h"
-#include "cefclient/renderer/client_app_renderer.h" 
-#include "cefclient/browser/browser_window.h"
-#include "cefclient/browser/browser_window_std_win.h"
-#include "cefclient/browser/main_context.h" 
-
- 
-delTraceBack notifyListener= NULL;
-client::MainContextImpl* mainContext;  
-client::MainMessageLoop* message_loop; 
- 
-
-managed_callback myMxCallback_;
-
-//1.
-int MyCefGetVersion()
-{	
-	 return 1004;
-}
-//2.
-int RegisterManagedCallBack(void* funcPtr,int callbackKind)
-{	
-	switch(callbackKind)
-	{
-		case 0:
-			{
-				notifyListener= (delTraceBack)funcPtr;		    
-				return 0;
-			}break;
-		case 1:
-			{
-			 
-			}break;
-		case 3:
-			{   
-				return 0;
-			}break;
-	} 
-	return 1;
-}
-//3.
-client::ClientApp* MyCefCreateClientApp()
-{	
-	// Parse command-line arguments.
-   CefRefPtr<CefCommandLine> command_line = CefCommandLine::CreateCommandLine();
-   command_line->InitFromString(::GetCommandLineW());
-	//create browser process first?
-   client::ClientApp* app= NULL;
-   client::ClientApp::ProcessType process_type = client::ClientApp::GetProcessType(command_line);
-   if (process_type == client::ClientApp::BrowserProcess)
-     app = new client::ClientAppBrowser();
-   else if (process_type == client::ClientApp::RendererProcess)
-     app = new client::ClientAppRenderer();
-   else if (process_type == client::ClientApp::OtherProcess)
-     app = new client::ClientAppOther();
-
->>>>>>> origin/v_dev
     // Create the main message loop object.
   message_loop = new client::MainMessageLoopStd();
   //message_loop.reset(new client::MainMessageLoopStd); 
  /* if (settings.multi_threaded_message_loop)
     message_loop.reset(new MainMessageLoopMultithreadedWin);
   else
-<<<<<<< HEAD
     message_loop.reset(new MainMessageLoopStd);*/
    return app;
 }
@@ -168,41 +94,14 @@ int MyCefInit(HINSTANCE hInstance,client::ClientApp* app)
 //6, 
 MyBrowser* MyCefCreateClientHandler()
 {	
-=======
-    message_loop.reset(new MainMessageLoopStd);*/
-   return app;
-}
-//4. 
-void MyCefClientAppSetManagedCallback(client::ClientApp* clientApp,managed_callback myMxCallback)
-{
-	myMxCallback_ = myMxCallback;
-}
-//5.
-int MyCefInit(HINSTANCE hInstance,client::ClientApp* app)
-{
-	CefRefPtr<client::ClientApp> myApp = app;   
-	mainContext= DllInitMain(hInstance,myApp); 
-	//set managed callback to
-	mainContext->myMxCallback_ = myMxCallback_;
-	return -1;
-}  
-//6, 
-MyBrowser* MyCefCreateClientHandler()
-{	
->>>>>>> origin/v_dev
 	/*const CefRect r(0,0,400,400); 
 	CefBrowserSettings settings;
 	client::MainContext::Get()->PopulateBrowserSettings(&settings);
     */
 	/*auto rr1= mainContext->GetRootWindowManager()->CreateRootWindow(false,false,r,"");*/	
 	 
-<<<<<<< HEAD
     //create root window handler?
 	auto myBw= new MyBrowser();
-=======
-    //create root window handler?
-	auto myBw= new MyBrowser();
->>>>>>> origin/v_dev
 	auto rootWindow= new client::RootWindowWin();  
 	myBw->rootWin = rootWindow;
 	
@@ -213,7 +112,6 @@ MyBrowser* MyCefCreateClientHandler()
 
 
 	//2. browser event handler
-<<<<<<< HEAD
 	auto hh = bwWindow->GetClientHandler();//  new client::ClientHandlerStd(bwWindow,"");
 	hh->MyCefSetManagedCallBack(myMxCallback_); 
 
@@ -222,16 +120,6 @@ MyBrowser* MyCefCreateClientHandler()
 
 //7.
 int MyCefSetupBrowserHwnd(MyBrowser* myBw,HWND surfaceHwnd,int x,int y,int w,int h,const wchar_t* url)
-=======
-	auto hh = bwWindow->GetClientHandler();//  new client::ClientHandlerStd(bwWindow,"");
-	hh->MyCefSetManagedCallBack(myMxCallback_); 
-
-	return myBw;
-}
-
-//7.
-int MyCefSetupBrowserHwnd(MyBrowser* myBw,HWND surfaceHwnd,int x,int y,int w,int h,const wchar_t* url)
->>>>>>> origin/v_dev
 {   
 
   // Information used when creating the native window.
@@ -271,7 +159,6 @@ int MyCefSetupBrowserHwnd(MyBrowser* myBw,HWND surfaceHwnd,int x,int y,int w,int
     url = "https://cefbuilds.com";*/
 
   // Create the first browser window.
-<<<<<<< HEAD
   //bool result= CefBrowserHost::CreateBrowser(window_info, handler.get(), url,                                browser_settings, NULL);
   bool result= CefBrowserHost::CreateBrowser(window_info, clientHandler, url,  browser_settings, NULL);
   if(result){
@@ -377,111 +264,4 @@ void PostData(MyBrowser* myBw, const wchar_t* url,const wchar_t* rawDataToPost,s
 	 myBw->bwWindow->GetBrowser()->GetMainFrame()->LoadRequest(request); 
 
 }
-=======
-  //bool result= CefBrowserHost::CreateBrowser(window_info, handler.get(), url,                                browser_settings, NULL);
-  bool result= CefBrowserHost::CreateBrowser(window_info, clientHandler, url,  browser_settings, NULL);
-  if(result){
-	  return 1;
-  }
-  else{
-	  return 0;
-  }  
-}
-//8.
-void MyCefDoMessageLoopWork()
-{		
-	CefDoMessageLoopWork();
-}
-//9.
-void MyCefShutDown(){
-	CefShutdown();
-}
-
-//part 2:
-//1. 
- void NativeMetSetResult(MethodArgs* args, int retIndex, jsvalue* value)
- {	 
-	 switch(retIndex){
-		 case 0:
-			args->result0 = *(value);
-			break;
-		 case 1:
-			 args->result1 = *(value);
-			 break;
-		 case 2:
-			 args->result2 = *(value);
-			 break;
-		 case 3:
-			 args->result3 = *(value);
-			 break;
-		 case 4:
-			 args->result4 = *(value);
-			 break;  
-
-	 }
- }
- //2.
- jsvalue MyCefNativeMetGetArgs(MethodArgs* args,int argIndex) 
- {
-	 switch(argIndex)
-	 {
-		case 0: return args->arg0;
-		case 1: return args->arg1;
-		case 2: return args->arg2;
-		case 3: return args->arg3;
-		case 4: return args->arg4;
-		default: 
-			{
-				jsvalue v;
-				v.type = JSVALUE_TYPE_EMPTY;
-				v.length =0;
-				return v;
-			} 
-	 } 
- }
- //3.
- void MyCefDisposePtr(void* ptr){
-	 delete ptr;
- }
-
-
-//---------------------------------------------------------------------------
-//part3:
-
-//1. 
-void NavigateTo(MyBrowser* myBw, const wchar_t* url){
-		 
-	 myBw->bwWindow->GetBrowser()->GetMainFrame()->LoadURL(url); 
-}
-//2.
-void ExecJavascript(MyBrowser* myBw, const wchar_t* jscode,const wchar_t* script_url){
-	
-	 myBw->bwWindow->GetBrowser()->GetMainFrame()->ExecuteJavaScript(jscode,script_url,0);
-}
-//3. 
-void PostData(MyBrowser* myBw, const wchar_t* url,const wchar_t* rawDataToPost,size_t rawDataLength){
-	
-	//create request
-	CefRefPtr<CefRequest> request(CefRequest::Create());
-	request->SetURL(url);
-	
-	//Add post data to request, the correct method and content-type header willbe set by CEF
-
-	CefRefPtr<CefPostDataElement> postDataElement(CefPostDataElement::Create());
-	postDataElement->SetToBytes(rawDataLength,rawDataToPost);
-	CefRefPtr<CefPostData> postData(CefPostData::Create());
-	postData->AddElement(postDataElement);
-	request->SetPostData(postData);
-
-	//add custom header
-	CefRequest::HeaderMap headerMap;
-	headerMap.insert(
-		std::make_pair("X-My-Header","My Header Value"));
-	request->SetHeaderMap(headerMap);
-
-	//load request
-	 myBw->bwWindow->GetBrowser()->GetMainFrame()->LoadRequest(request); 
-
-}
->>>>>>> origin/v_dev
  
--- cef3/cefclient/ExportFuncs.h
+++ cef3/cefclient/ExportFuncs.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "dll_init.h"
 #include "common/client_app.h" 
 #include "browser/client_handler.h"
@@ -67,74 +66,4 @@ extern "C"{
 	//----------------------------
 	
 
-=======
-#include "dll_init.h"
-#include "common/client_app.h" 
-#include "browser/client_handler.h"
-#include "browser/client_handler_std.h"
-#include "browser/root_window_win.h"
-
-#define MY_DLL_EXPORT __declspec(dllexport)  
- 
-
-extern "C"{  	 
-	 
-
-	class MyBrowser
-	{
-	public:
-		    client::RootWindowWin* rootWin;
-		    client::BrowserWindow* bwWindow; 
-	};
-
-
-	//part 1
-
-	//1.
-	MY_DLL_EXPORT int MyCefGetVersion(); 
-	//2.
-	MY_DLL_EXPORT int RegisterManagedCallBack(void* callback,int callBackKind); 
-	//3. 
-	MY_DLL_EXPORT client::ClientApp* MyCefCreateClientApp();	
-	//4.
-	MY_DLL_EXPORT int MyCefInit(HINSTANCE hInstance,client::ClientApp* app);
-	//5.
-	MY_DLL_EXPORT void MyCefClientAppSetManagedCallback(client::ClientApp* clientApp,managed_callback myMxCallback);
-	
-	
-	//6.
-	MY_DLL_EXPORT MyBrowser* MyCefCreateClientHandler();
-	//7.
-	MY_DLL_EXPORT int MyCefSetupBrowserHwnd(MyBrowser* myBw,HWND surfaceHwnd,int x,int y,int w,int h,const wchar_t* url);
-
-	//8.
-	MY_DLL_EXPORT void MyCefDoMessageLoopWork(); 
-	 
-	//9.
-	MY_DLL_EXPORT void MyCefShutDown();  
-
-	
-	//--------------------
-	//part 2
-	//1.
-	MY_DLL_EXPORT  void NativeMetSetResult(MethodArgs* args, int retIndex, jsvalue* value);
-	//2.
-	MY_DLL_EXPORT jsvalue MyCefNativeMetGetArgs(MethodArgs* args,int argIndex);
-	//3.
-	MY_DLL_EXPORT void MyCefDisposePtr(void* ptr);
-
-
-	//part3:
-	//--------------------
-	//1.
-	MY_DLL_EXPORT void NavigateTo(MyBrowser* myBw, const wchar_t* url);
-	//2. 
-	MY_DLL_EXPORT void ExecJavascript(MyBrowser* myBw, const wchar_t* jscode,const wchar_t* script_url);
-	//3.
-	MY_DLL_EXPORT void PostData(MyBrowser* myBw, const wchar_t* url,const wchar_t* rawDataToPost,size_t rawDataLength);
-
-	//----------------------------
-	
-
->>>>>>> origin/v_dev
 }
