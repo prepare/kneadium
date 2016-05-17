4491a9b16db64a00bf942660f768b9f44e812ff7
win <wintercoredev@gmail.com>
2016-05-17 15:16:45 +0700

    157.1: move code

---
+++ NativePatcher/BridgeBuilder/ApiModel/ApiBuild.cs
@@ -0,0 +1,40 @@
+﻿//2016, MIT, WinterDev
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+namespace BridgeBuilder
+{
+    class ApiBuilder
+    {
+
+        public void Build(string apiFolder)
+        {
+            List<string> onlyHeaders = new List<string>();
+            string[] files = Directory.GetFiles(apiFolder);
+            foreach (var filename in files)
+            {
+                if (filename.EndsWith(".h"))
+                {
+                    if (Path.GetFileName(filename) == "ctocpp.h")
+                    {
+                        //skip this
+                        continue;
+                    }
+                    onlyHeaders.Add(filename);
+                }
+            }
+
+            List<CodeCompilationUnit> compilationUnits = new List<CodeCompilationUnit>();
+            foreach (var filename in onlyHeaders)
+            {
+                var headerParser = new HeaderFileParser();
+                headerParser.Parse(filename);
+                compilationUnits.Add(headerParser.Result);
+            }
+        }
+
+
+    }
+}
