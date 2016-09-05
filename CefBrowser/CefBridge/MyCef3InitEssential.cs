6e00cf258ca7cf574238f585aab5685d2a6ca683
win <wintercoredev@gmail.com>
2016-09-05 20:29:27 +0700

    214: separate WinFormsMsgLoopPump

--- CefBrowser/CefBridge/MyCef3InitEssential.cs
+++ CefBrowser/CefBridge/MyCef3InitEssential.cs
@@ -87,7 +87,7 @@ namespace LayoutFarm.CefBridge
         }
         public override IWindowForm CreateNewBrowserWindow(int width, int height)
         {
-            Form1 form1 = new Form1();
+            Form form1 = new Form();
             form1.Width = width;
             form1.Height = height;
             return MyWindowForm.TryGetWindowControlOrRegisterIfNotExists(form1);
@@ -184,52 +184,6 @@ namespace LayoutFarm.CefBridge
         }
     }
 
-    static class WinFormCefMsgLoopPump
-    {
 
-        static Timer loopTimer = new Timer();
-        static Form tinyForm;
-        static IntPtr tinyFormHandle;
-        public static void Start()
-        {
-            if (tinyForm == null)
-            {
-                tinyForm = new System.Windows.Forms.Form();
-                tinyForm.Size = new System.Drawing.Size(10, 10);
-                tinyForm.Visible = false;
-            }
-            //force it create handle****  
-            tinyFormHandle = tinyForm.Handle;
-
-            //Cef3's message pump ***
-            //---------------------------
-            //this is CefMsgLoopPump implementation
-            //it should not too fast or too slow to call  
-            bool appIsIdel = true;
-            loopTimer.Interval = 30;//30fps
-            loopTimer.Tick += (s, e) =>
-            {
-                if (appIsIdel)
-                {
-                    for (int i = 10; i >= 0; --i)
-                    {
-                        MyCef3InitEssential.CefDoMessageLoopWork();
-                    }
-                    appIsIdel = false;
-                }
-            };
-
-            Application.Idle += (s, e) =>
-            {
-                appIsIdel = true;
-            };
-
-            loopTimer.Enabled = true;
-        }
-        public static void SafeUIInvoke(SimpleDel simpleDel)
-        {
-            tinyForm.Invoke(simpleDel);
-        }
-    }
 
 }
