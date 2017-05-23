7b1cc34419bcc504f5d655083f8b01d8cfacfccb
win <wintercoredev@gmail.com>
2017-05-24 03:13:54 +0700

    258: update to 3.3029

--- NativePatcher/BridgeBuilder/Form1.cs
+++ NativePatcher/BridgeBuilder/Form1.cs
@@ -82,12 +82,14 @@ namespace BridgeBuilder
             //string srcRootDir = @"D:\projects\cef_binary_3.2704.1418"; 
             //string srcRootDir = @"D:\projects\cef_binary_3.2785.1466";  
             //string srcRootDir = @"D:\projects\cef_binary_3.2883.1548\\tests"; 
-            string srcRootDir = @"D:\projects\cef_binary_3.2883.1553\\tests";
+            //string srcRootDir = @"D:\projects\cef_binary_3.2883.1553\\tests";
+            string srcRootDir = @"D:\projects\cef_binary_3.3029.1619\tests";
             string saveFolder = "d:\\WImageTest\\cefbridge_patches";
 
             PatchBuilder builder2 = new PatchBuilder(new string[]{
                 srcRootDir,
-                @"D:\projects\cef_binary_3.2883.1553\\shared"
+                //@"D:\projects\cef_binary_3.2883.1553\\shared"
+                @"D:\projects\cef_binary_3.3029.1619\shared"
             });
             builder2.LoadPatchesFromFolder(saveFolder);
 
