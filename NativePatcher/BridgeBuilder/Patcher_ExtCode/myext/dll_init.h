f2c8db0b533f20cf55a4107bf641cd5b96c716d0
win <wintercoredev@gmail.com>
2017-07-18 10:52:06 +0700

    340: move some code

--- NativePatcher/BridgeBuilder/Patcher_ExtCode/myext/dll_init.h
+++ NativePatcher/BridgeBuilder/Patcher_ExtCode/myext/dll_init.h
@@ -1,17 +1,6 @@
 //MIT, 2015-2017, WinterDev 
-#include "include/base/cef_scoped_ptr.h"
-#include "include/cef_command_line.h"
-#include "include/cef_sandbox_win.h"
-#include "tests/shared/browser/client_app_browser.h"
-#include "tests/cefclient/browser/main_context_impl.h"
-#include "tests/cefclient/browser/main_message_loop_multithreaded_win.h"
-#include "tests/shared/browser/main_message_loop_std.h"
-#include "tests/cefclient/browser/root_window_manager.h"
-#include "tests/cefclient/browser/test_runner.h"
-#include "tests/shared/common/client_app_other.h"
-#include "tests/shared/renderer/client_app_renderer.h"
-#include "mycef.h"
-
+#include "include/base/cef_scoped_ptr.h" 
+#include "tests/cefclient/browser/main_context_impl.h" 
 namespace client {
 	namespace init_main {
 		void SetManagedCallback(managed_callback callback);
