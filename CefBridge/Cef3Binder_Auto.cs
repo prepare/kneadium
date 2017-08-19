7899789d42b58b5b84bcd2b82fe32ee981965405
win <wintercoredev@gmail.com>
2017-08-19 10:32:37 +0700

    613: move code

--- CefBridge/Cef3Binder_Auto.cs
+++ CefBridge/Cef3Binder_Auto.cs
@@ -60,39 +60,7 @@ namespace LayoutFarm.CefBridge
     }
     static partial class Cef3Binder
     {
-        public static void MyCefBwCall(IntPtr myCefBw, CefBwCallMsg methodName, int value)
-        {
-            JsValue ret;
-            //
-            JsValue arg1 = new JsValue();
-            arg1.Type = JsValueType.Integer;
-            arg1.I32 = value;
-            //
-            JsValue arg2 = new JsValue();
-
-            MyCefBwCall2(myCefBw, (int)methodName, out ret, ref arg1, ref arg2);
-        }
-
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern MyCefCallback MyCefJsValueGetManagedCallback(ref JsValue v);
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void MyCefJsValueSetManagedCallback(ref JsValue v, MyCefCallback cb);
-
-
-
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void MyCefBwCall2(IntPtr myCefBw, int methodName, out JsValue ret, ref JsValue arg1, ref JsValue arg2);
-
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void MyCefFrameCall2(IntPtr myCefBw, int methodName, out JsValue ret, ref JsValue arg1, ref JsValue arg2);
-
-
-
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void MyCefMet_CefBrowser(IntPtr /*cef_browser_t* */ me1, int metName, out JsValue ret, ref JsValue arg1, ref JsValue arg2, ref JsValue arg3, ref JsValue arg4, ref JsValue arg5, ref JsValue arg6);
-        [DllImport(CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void MyCefMet_CefFrame(IntPtr /*cef_frame_t**/ me1, int metName, out JsValue ret, ref JsValue arg1, ref JsValue arg2, ref JsValue arg3, ref JsValue arg4, ref JsValue arg5, ref JsValue arg6);
-
+       
 
         [DllImport(Cef3Binder.CEF_CLIENT_DLL, CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr NewInstance(int typeName, MyCefCallback callback, ref JsValue v1);
