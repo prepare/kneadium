373d6c654265a8c0ebae5586b94fd40bbb87fc4e
win <wintercoredev@gmail.com>
2017-08-22 19:13:22 +0700

    642: rename folders

---
+++ CefBrowser/CefImpl/CefWebBrowser.cs
@@ -0,0 +1,162 @@
+﻿//BSD, 2015-2017, WinterDev
+// Copyright © 2010-2014 The CefSharp Authors. All rights reserved. 
+// Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+namespace LayoutFarm.CefBridge
+{
+    public sealed class CefWebBrowserControl : Control
+    {
+        MyCefBrowser cefBrowser;
+        IWindowControl thisWindowControl;
+        public event EventHandler BrowserReady;
+        public CefWebBrowserControl()
+        {
+            SetStyle(
+                ControlStyles.ContainerControl
+                | ControlStyles.ResizeRedraw
+                | ControlStyles.FixedWidth
+                | ControlStyles.FixedHeight
+                | ControlStyles.StandardClick
+                | ControlStyles.UserMouse
+                | ControlStyles.SupportsTransparentBackColor
+                | ControlStyles.StandardDoubleClick
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.CacheText
+                | ControlStyles.EnableNotifyMessage
+                | ControlStyles.DoubleBuffer
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.UseTextForAccessibility
+                | ControlStyles.Opaque,
+                false);
+            SetStyle(
+                ControlStyles.UserPaint
+                | ControlStyles.AllPaintingInWmPaint
+                | ControlStyles.Selectable,
+                true);
+            thisWindowControl = MyWindowControl.TryGetWindowControlOrRegisterIfNotExists(this);
+        }
+        protected override void OnPreviewKeyDown(PreviewKeyDownEventArgs e)
+        {
+            base.OnPreviewKeyDown(e);
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                case Keys.Down:
+                case Keys.Left:
+                case Keys.Right:
+                    e.IsInputKey = true;
+                    break;
+            }
+        }
+        public void NavigateTo(string url)
+        {
+            if (cefBrowser != null)
+            {
+                this.cefBrowser.NavigateTo(url);
+            }
+        }
+        public MyCefBrowser Agent
+        {
+            get { return this.cefBrowser; }
+        }
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            if (cefBrowser != null)
+            {
+                cefBrowser.SetSize(this.Width, this.Height);
+            }
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            if (!DesignMode)
+            {
+                InitWebBrowserControl();
+            }
+        }
+        void InitWebBrowserControl()
+        {
+            //---------
+            //create timer for notify when browser is ready
+            var bwReadyTimer = new Timer();
+            bwReadyTimer.Interval = 50;//ms
+            bwReadyTimer.Tick += (s, e1) =>
+            {
+                if (this.IsHandleCreated && this.Agent.IsBrowserCreated)
+                {
+                    bwReadyTimer.Enabled = false;//stop timer
+                    if (cefBrowser != null)
+                    {
+                        cefBrowser.SetSize(this.Width, this.Height);
+                    }
+
+                    if (BrowserReady != null)
+                    {
+                        BrowserReady(this, EventArgs.Empty);
+                    }
+                    //we use this timer once
+                    //so...
+                    //after notify we delete it
+                    bwReadyTimer.Dispose();
+                    bwReadyTimer = null;
+                }
+            };
+            bwReadyTimer.Enabled = true;
+
+            //---------
+            //create cef browser when handle is created 
+            MyCefOsrListener osrListener = new MyCefOsrListener();
+            //this.cefBrowser = new MyCefBrowser(thisWindowControl, 0, 0, 800, 500, "about:blank", true) { OsrListener = osrListener };
+            this.cefBrowser = new MyCefBrowser(thisWindowControl, 0, 0, 800, 500, "about:blank", false);
+
+        }
+
+        class MyCefOsrListener : CefOsrListener
+        {
+            //test output
+            int n = 0;
+
+            public override void OnRender(NativeCallArgs args)
+            {
+                //if not set to 0
+                //images not render to native cef win                 
+
+                return; //just return
+                if (n > 100) return;
+                //----------------------
+
+                //INIT_MY_MET_ARGS(metArgs, 3) 
+                //MyCefSetVoidPtr2(&vargs[1], buffer);
+                //MyCefSetInt32(&vargs[2], width);
+                //MyCefSetInt32(&vargs[3], height);
+
+                IntPtr rawBitBuffer = args.GetArgAsNativePtr(1);
+                int width = args.GetArgAsInt32(2);
+                int height = args.GetArgAsInt32(3);
+
+                unsafe
+                {
+                    using (Bitmap bmp = new Bitmap(width, height))
+                    {
+                        var bmpdata = bmp.LockBits(
+                            new Rectangle(0, 0, width, height),
+                            System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                            System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+                        CopyMemory(bmpdata.Scan0, rawBitBuffer, width * height * 4);
+                        bmp.UnlockBits(bmpdata);
+                        bmp.Save(ReferencePaths.SAVE_IMAGE_PATH + (n++) + ".jpg");
+                    }
+                }
+            }
+
+            [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+            static extern void CopyMemory(IntPtr dest, IntPtr src, int count);
+        }
+    }
+}
