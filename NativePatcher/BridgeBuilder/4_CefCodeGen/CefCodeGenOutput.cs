dad32453888f7805aaf1c97550a0c1a89ecd776f
win <wintercoredev@gmail.com>
2017-09-18 21:43:19 +0700

    691: add notes

--- NativePatcher/BridgeBuilder/4_CefCodeGen/CefCodeGenOutput.cs
+++ NativePatcher/BridgeBuilder/4_CefCodeGen/CefCodeGenOutput.cs
@@ -1,13 +1,13 @@
 ﻿//MIT, 2016-2017 ,WinterDev
- 
+
 namespace BridgeBuilder
 {
     class CefCodeGenOutput
     {
-        internal CodeStringBuilder _cppHeaderExportFuncAuto = new CodeStringBuilder();
-        internal CodeStringBuilder _cppHeaderInternalForExportFuncAuto = new CodeStringBuilder();
-        internal CodeStringBuilder _cppCode = new CodeStringBuilder();
-        internal CodeStringBuilder _csCode = new CodeStringBuilder();
+        internal CodeStringBuilder _cppHeaderExportFuncAuto = new CodeStringBuilder();//ExportFuncAuto.h
+        internal CodeStringBuilder _cppHeaderInternalForExportFuncAuto = new CodeStringBuilder(); //InternalHeaderForExportFunc.h
+        internal CodeStringBuilder _cppCode = new CodeStringBuilder(); //ExportFuncAuto.cpp
+        internal CodeStringBuilder _csCode = new CodeStringBuilder(); //
 
     }
 }
