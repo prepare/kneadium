2dec55857eac54a78ae7d5fdc688660400e0501b
win <wintercoredev@gmail.com>
2017-09-18 21:39:25 +0700

    690: arrange code

---
+++ NativePatcher/BridgeBuilder/4_CefCodeGen/CefCodeGenOutput.cs
@@ -0,0 +1,13 @@
+﻿//MIT, 2016-2017 ,WinterDev
+ 
+namespace BridgeBuilder
+{
+    class CefCodeGenOutput
+    {
+        internal CodeStringBuilder _cppHeaderExportFuncAuto = new CodeStringBuilder();
+        internal CodeStringBuilder _cppHeaderInternalForExportFuncAuto = new CodeStringBuilder();
+        internal CodeStringBuilder _cppCode = new CodeStringBuilder();
+        internal CodeStringBuilder _csCode = new CodeStringBuilder();
+
+    }
+}
