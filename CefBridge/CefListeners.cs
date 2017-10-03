035a0c808469ad948487f0595632bd0bb4a52f61
win <wintercoredev@gmail.com>
2017-10-03 18:20:03 +0700

    764: remove unused code

--- CefBridge/CefListeners.cs
+++ CefBridge/CefListeners.cs
@@ -2,22 +2,8 @@
 
 namespace LayoutFarm.CefBridge
 {
-
-
     public abstract class CefOsrListener
     {
         public virtual void OnRender(NativeCallArgs args) { }
-
-    }
-   
-
-    public class MyCefContextArgs
-    {
-
-        public readonly NativeJsContext context;
-        public MyCefContextArgs(NativeCallArgs args)
-        {
-            context = new NativeJsContext(args.GetArgAsNativePtr(4));
-        }
     }
 }
