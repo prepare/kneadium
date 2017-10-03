948ab30658b03799dc383f535516a52c30ee6e13
win <wintercoredev@gmail.com>
2017-10-03 21:20:23 +0700

    767.3

--- NativePatcher/dev_snap_win32/tests/cefclient/myext/mycef_msg_const.h
+++ NativePatcher/dev_snap_win32/tests/cefclient/myext/mycef_msg_const.h
@@ -2,13 +2,13 @@
 #pragma once 
 //----------------------------------------------------------------------
 //my custom msgs
- 
+
 const int CEF_MSG_ClientHandler_NotifyBrowserClosing = 100;
 const int CEF_MSG_ClientHandler_NotifyBrowserClosed = 101;
 const int CEF_MSG_ClientHandler_NotifyBrowserCreated = 102;
 
 
- 
+
 const int CEF_MSG_ClientHandler_CloseDevTools = 108;
 
 const int CEF_MSG_ClientHandler_SetResourceManager = 140;
@@ -19,12 +19,11 @@ const int CEF_MSG_BinaryResouceProvider_OnRequest = 145;
 const int CEF_MSG_CefSettings_Init = 150;
 const int CEF_MSG_MainContext_GetConsoleLogPath = 151;
 const int CEF_MSG_OSR_Render = 155;
- 
+
 const int CEF_MSG_OnQuery = 205;
 
 
 const int CEF_MSG_MyV8ManagedHandler_Execute = 301;
-const int CEF_MSG_MyCefDomGetTextWalk_Visit = 302;
 const int CEF_MSG_HereOnRenderer = 303;
 
 const int CEF_MSG_ClientHandler_NotifyTitle = 502;
