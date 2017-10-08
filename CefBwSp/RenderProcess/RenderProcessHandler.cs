18b6c20e56552a88104c23a20b6f58f8f56fa612
win <wintercoredev@gmail.com>
2017-10-08 18:41:34 +0700

    805: test accessor

--- CefBwSp/RenderProcess/RenderProcessHandler.cs
+++ CefBwSp/RenderProcess/RenderProcessHandler.cs
@@ -38,7 +38,7 @@ namespace LayoutFarm.CefBridge
         public void OnContextCreated(CefRenderProcessHandler.OnContextCreatedArgs args)
         {
 
-            //eg  "<html><head><script>function docload(){ console.log(test001());console.log(test_myobj[12345]); console.log(test_myobj.myprop);}</script><body onload=\"docload()\"><h1>hello!</h1></body></html>"
+            //eg  "<html><head><script>function docload(){ console.log(test001()); console.log(test_myobj.myprop);console.log(test_myobj[12345]);}</script><body onload=\"docload()\"><h1>hello!</h1></body></html>"
 
             dbugRenderProcessLog.WriteLine("context_created");
             CefV8Context context = args.context();
@@ -50,6 +50,10 @@ namespace LayoutFarm.CefBridge
 
             CefV8Accessor accessor = CefV8Accessor.New((id, argPtr) =>
             {
+                //--------------------------
+                //this shows the unsafe and low-level interface to native.
+                //you can use a 'higher' level wrapper.
+                //--------------------------
 
                 //from https://github.com/v8/v8/wiki/Embedder%27s-Guide
                 //accessor callbacks are invoked when a specific object property is accessed by a script
@@ -60,8 +64,9 @@ namespace LayoutFarm.CefBridge
 
                 CefV8Accessor.GetArgs arg = new CefV8Accessor.GetArgs(argPtr);
                 arg.retval(CefV8Value.CreateString("hello! from accessor").nativePtr);
+                arg.myext_setRetValue(true);//finish the accessor!
             });
-            
+
             CefV8Interceptor intercepter = CefV8Interceptor.New((id, argPtr) =>
             {
                 //from https://github.com/v8/v8/wiki/Embedder%27s-Guide
@@ -72,6 +77,11 @@ namespace LayoutFarm.CefBridge
                 //  You can also specify a callback for whenever a script accesses any object property. These are called interceptors. For efficiency, there are two types of interceptors:
                 //  named property interceptors - called when accessing properties with string names. An example of this, in a browser environment, is document.theFormName.elementName.
                 //  indexed property interceptors - called when accessing indexed properties. An example of this, in a browser environment, is document.forms.elements[0].
+
+                //--------------------------
+                //this shows the unsafe and low-level interface to native.
+                //you can use a 'higher' level wrapper.
+                //--------------------------
 
 
                 if ((id >> 16) != 41) return;
@@ -101,8 +111,11 @@ namespace LayoutFarm.CefBridge
             //--------
             //
             CefV8Accessor empty = new CefV8Accessor();
-            
-            CefV8Value cef_object = CefV8Value.CreateObject(empty, intercepter);
+            CefV8Interceptor empty_interceptor = new CefV8Interceptor();
+            CefV8Value cef_object = CefV8Value.CreateObject(accessor, empty_interceptor);
+            //if you want to use accessor, your must set it with SetValue() like this
+            cef_object.SetValue("myprop", cef_v8_accesscontrol_t.V8_ACCESS_CONTROL_DEFAULT, cef_v8_propertyattribute_t.V8_PROPERTY_ATTRIBUTE_NONE);
+
             //
             //set to global object
             cefV8Global.SetValue("test_myobj", cef_object, cef_v8_propertyattribute_t.V8_PROPERTY_ATTRIBUTE_READONLY);
